34249d30ce3d9e71a6ba26bc72897ce225197ccc
Mayavan <mayavan95@gmail.com>
2018-12-22 21:24:10 -0500

    Finsihed photography page

---
+++ gallery.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <!-- 
+	//////////////////////////////////////////////////////
+
+	Hurricane Website
+	DESIGNED & DEVELOPED by Mayavan
+		
+	Website: 		http://yihanpeng.com/
+	Email: 			mayavan95@gmail.com
+
+	//////////////////////////////////////////////////////
+	-->
+    <title>Yihan Peng</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<?php	
+		session_start();
+		// get category	
+		$category = $_POST['category'];
+		
+		if($category=="1")
+		{
+        ?>
+            <div class="col-md-3">
+                <a href="images/image_1.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_1.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_2.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_2.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_3.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_3.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_4.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_4.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="images/image_1.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_1.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_2.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_2.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_3.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_3.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_4.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_4.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+        <?php
+		}
+		else if($category=="2")
+		{
+            ?>
+            <div class="col-md-3">
+                <a href="images/image_1.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_3.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_2.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_2.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_3.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_1.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_4.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_4.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="images/image_1.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_1.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_2.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_2.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_3.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_3.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_4.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_4.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+        <?php
+		}		
+		else if($category=="3")
+		{
+            ?>
+            <div class="col-md-3">
+                <a href="images/image_1.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_6.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_2.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_7.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_3.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_8.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_4.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_9.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="images/image_1.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_11.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_2.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_2.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_3.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_3.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+            <div class="col-md-3">
+            <a href="images/image_4.jpg" class="photo img d-flex justify-content-center align-items-center" style="background-image: url(images/image_4.jpg);">
+                    <div class="overlay"></div>
+                </a>
+            </div>
+        <?php
+		}
+		
+	?>
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- Flexslider -->
+    <script src="js/jquery.flexslider-min.js"></script>
+    <!-- Stellar -->
+    <script src="js/jquery.stellar.min.js"></script>
+
+
+    <!-- MAIN JS -->
+    <script src="js/main.js"></script>
+
+    <!-- Custom JS -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
