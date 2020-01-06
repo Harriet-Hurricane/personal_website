ea05980f18b90757112c7cf1c8e54d8696f08edd
Mayavan <mayavan95@gmail.com>
2020-01-05 23:38:01 -0500

    Minor aesthetics changes

--- project_memo.php
+++ project_memo.php
@@ -72,8 +72,10 @@
                     <li><a href="index.html">Home</a></li>
                     <li><a href="about.html">About</a></li>
                     <li><a href="resume.html">Résumé</a></li>
+                    <hr width="50%">
                     <li class="colorlib-active"><a href="projects.html">Projects</a></li>
                     <li><a href="visualizer.html">Data Visualization</a></li>
+                    <hr width="50%">
                     <li><a href="videocreation.html">Video Creation</a></li>
                     <li><a href="Pictures.html">Pictures</a></li>
                     <li><a href="contact.html">Contact</a></li>
